06fa51455a9fc999f9c311e84bb489ac47811207
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-06-12 15:08:57 +0100

    fix path

--- ecommerce-front/src/components/layout/Navbar.jsx
+++ ecommerce-front/src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container, Button, Modal, Form, Alert } from 'react-bootstrap';
-import { getCart } from '../services/api';
+import { getCart } from '../../services/api';
 
 const NavigationBar = () => {
   const navigate = useNavigate();
